729b8ba6e7f0cb87b230b50639e6dabbc15d131a
tyrenix <daanila01@gmail.com>
2025-04-14 09:07:09 +0400

    feat: support string base errors and improve option handling

--- builder.go
+++ builder.go
@@ -1,23 +1,40 @@
 package goerr
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // New creates a new Error instance by wrapping a base error,
 // optional additional errors, strings, or configuration options.
-func New(base error, opts ...any) error {
-	if base == nil {
+func New(base any, opts ...any) error {
+	if base == nil || base == "" {
 		return nil
 	}
 
+	// create new error
 	goerr := &Error{}
 
-	// If base is already a *goerr.Error, copy its httpCode
-	if be, ok := base.(*Error); ok {
-		goerr.httpCode = be.httpCode
+	// parse base
+	switch v := base.(type) {
+	case *Error:
+		goerr.err = v.err
+		goerr.httpCode = v.httpCode
+	case string:
+		goerr.err = errors.New(v)
+	case error:
+		goerr.err = v
+	default:
+		return fmt.Errorf("goerr: unsupported option type %T", v)
 	}
 
-	// Merge errors and apply options
-	joined := base
+	// // if base is already a *goerr.Error, copy its httpCode
+	// if be, ok := base.(*Error); ok {
+	// 	goerr.httpCode = be.httpCode
+	// }
+
+	// merge errors and apply options
+	joined := goerr.err
 	for _, opt := range opts {
 		switch v := opt.(type) {
 		case string:
@@ -31,6 +48,9 @@ func New(base error, opts ...any) error {
 		}
 	}
 
+	// set error
 	goerr.err = joined
+
+	// return error
 	return goerr
 }
